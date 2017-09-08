5a9648549634d8655ec336b311f01cdb08cac6c5
xbwangh <xbwangh@isoftstone.com>
2017-09-08 10:38:08 +0800

    提交测试代码

--- user_app/models.py
+++ user_app/models.py
@@ -21,14 +21,13 @@ class User(models.Model):
     user_phone = models.CharField(max_length=11, verbose_name=u'手机号码')
     user_address = models.CharField(max_length=256, verbose_name=u'家庭住址', default=0)
     user_password = models.CharField(max_length=256, verbose_name=u'用户密码', null=True)
-    department_id = models.CharField(max_length=64, verbose_name=u'部门ID')
     register_time = models.DateTimeField(auto_now_add=True, verbose_name=u'注册时间')
     user_status = models.IntegerField(choices=user_status, verbose_name=u'状态',  default=0)
 
     class Meta:
         verbose_name = u'用户表'
         verbose_name_plural = verbose_name
-        db_table = 'user'
+        db_table = 'test_user'
 
     def __unicode__(self):
         return self
