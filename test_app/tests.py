638acbe691d655e8f9480f344c1087c4a56ac2f2
xbwangh <xbwangh@isoftstone.com>
2017-09-08 10:43:00 +0800

    读取excel文件

--- test_app/tests.py
+++ test_app/tests.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.test import TestCase
-
-# Create your tests here.
+import os
+import jpype
+from jpype import *
+jvmPath = jpype.getDefaultJVMPath()
+if not jpype.isJVMStarted():
+    jpype.startJVM(jvmPath, "-ea", "-Djava.class.path=%s" % ("../jar/ReadExcel.jar"))
+jpype.java.lang.System.out.println("hello world!")
+JDClass = JClass("com.test.ReadExcel")
+jd = JDClass()
+result = jd.read("wangbo")
+print(result)
+jpype.shutdownJVM()
